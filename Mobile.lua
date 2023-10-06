822fb7319922a0059981dcd85fe2ffab0f335266
xshiba <92438125+xshiba@users.noreply.github.com>
2023-10-07 00:07:07 +0700

    Update Mobile.lua

--- Mobile.lua
+++ Mobile.lua
@@ -11,7 +11,7 @@ if game.PlaceId==2753915549 or game.PlaceId==4442272183 or game.PlaceId==7449423
     end 
 elseif game.PlaceId==4520749081 or game.PlaceId==6381829480 or game.PlaceId==5931540094 or game.PlaceId==6596144663 then 
     loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/CreamSoScute/main/LoadKL.lua",true))()
-elseif game.PlaceId==6329844902 or game.PlaceId==12192552089 or game.PlaceId==6299805723 or game.PlaceId==914010731 or game.PlaceId==6918802270 then 
+elseif game.PlaceId==6329844902 or game.PlaceId==12192552089 or game.PlaceId==6299805723 or game.PlaceId==914010731 or game.PlaceId==6918802270 or game.PlaceId==14979402479 then 
     loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/CreamSoScute/main/LoadAm.lua",true))()
 else game.Players.LocalPlayer:Kick("\224\185\132\224\184\161\224\185\136\224\184\158\224\184\154\224\185\129\224\184\161\224\184\158")
     wait(1)game:Shutdown()
